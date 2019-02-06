a060fcf5be01b8b87f72c18a68731c42e01c5c3a
Maskim Burnin <Maksim.Burnin@gmail.com>
2019-02-06 15:49:35 +0500

    updated zend_execute path

--- Formula/php@5.6.rb
+++ Formula/php@5.6.rb
@@ -481,7 +481,7 @@ diff --git a/Zend/zend_execute.h b/Zend/zend_execute.h
 index a7af67bc13..ae71a5c73f 100644
 --- a/Zend/zend_execute.h
 +++ b/Zend/zend_execute.h
-@@ -71,6 +71,15 @@ ZEND_API int zend_eval_stringl_ex(char *str, int str_len, zval *retval_ptr, char
+@@ -71,4 +71,12 @@ ZEND_API int zend_eval_stringl_ex(char *str, int str_len, zval *retval_ptr, char
  ZEND_API char * zend_verify_arg_class_kind(const zend_arg_info *cur_arg_info, ulong fetch_type, const char **class_name, zend_class_entry **pce TSRMLS_DC);
  ZEND_API int zend_verify_arg_error(int error_type, const zend_function *zf, zend_uint arg_num, const char *need_msg, const char *need_kind, const char *given_msg, const char *given_kind TSRMLS_DC);
 +static zend_always_inline temp_variable *EX_TMP_VAR(void *ex, int n)
@@ -492,7 +492,5 @@ index a7af67bc13..ae71a5c73f 100644
 +{
 +	return (temp_variable *)((zend_uintptr_t)ex - (1 + n) * sizeof(temp_variable));
 +}
-+
+ 
  static zend_always_inline void i_zval_ptr_dtor(zval *zval_ptr ZEND_FILE_LINE_DC TSRMLS_DC)
- {
-	if (!Z_DELREF_P(zval_ptr)) {
